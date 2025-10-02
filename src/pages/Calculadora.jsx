7492d05a96b6f6bcf57da942704345b004bf24ce
ERIK SILVERIO TOMAZOLI <erik.stomazoli@senacsp.edu.br>
2025-10-01 22:24:11 -0300

    Terminar a calculadora

--- src/pages/Calculadora.jsx
+++ src/pages/Calculadora.jsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
 
 export default function Calculadora(){
+    const [primeiro, setPrimeiro] = useState('')
+    const [segundo, setSegundo] = useState('')
+    const [resultado, setResultado] = useState(null)
 
+    console.log(primeiro,segundo)
+        const somar= ()=>{
+            //realizar a soma de primeiro + segundo
+            //atribuir ao resultado
+        }
     return(
          <div className="container mt-5" style={{ maxWidth: "500px" }}>
             <h1 className="text-center mb-4">Calculadora</h1>
 
-            <input type="number"className="me-2"/>
+            <input 
+            type="number"
+            className="me-2" 
+            value={primeiro}
+            onChange={(e) => setPrimeiro(e.target.value)}
+            />
 
-            <input type="number"className="me-2"/>
-            
-            <button>Somar</button>
+            <input 
+            type="number"
+            className="me-2" 
+            value={segundi}
+            onChange={(e)=> setSegundo(e.target.value)}
+            />
+
+            <button onClick={somar}>Somar</button>
 
             <div>Resultado</div>
         </div>
